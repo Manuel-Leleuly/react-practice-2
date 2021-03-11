ac66c2f35653632dcc598a703a41b8f5f8d954d6
Manuel-Leleuly <manuel.leleuly@gmail.com>
2021-03-11 21:45:04 +0700

    first exercise

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -13,7 +13,12 @@ class DishDetail extends Component {
           <div key={comment.id} className="text-left mx-2">
             <CardText>{comment.comment}</CardText>
             <CardText className="mb-3">
-              -- {comment.author}, {comment.date}
+              -- {comment.author},{" "}
+              {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "2-digit",
+              }).format(new Date(Date.parse(comment.date)))}
             </CardText>
           </div>
         );
@@ -36,31 +41,44 @@ class DishDetail extends Component {
     }
   }
 
-  render() {
-    const dishDetailComponent = (
-      <div className="col-12 col-md-5 m-1" key={this.props.dishDetail.id}>
-        <Card>
-          <CardImg
-            width="100%"
-            src={this.props.dishDetail.image}
-            alt={this.props.dishDetail.name}
-          />
-          <CardTitle className="text-left mx-2 font-weight-bold">
-            {this.props.dishDetail.name}
-          </CardTitle>
-          <CardText className="text-left mx-2">
-            {this.props.dishDetail.description}
-          </CardText>
-        </Card>
-      </div>
-    );
+  renderDishDetail(dishDetail) {
+    if (dishDetail !== null) {
+      const dishDetailComponent = (
+        <div className="col-12 col-md-5 m-1" key={dishDetail.id}>
+          <Card>
+            <CardImg
+              width="100%"
+              src={dishDetail.image}
+              alt={dishDetail.name}
+            />
+            <CardTitle className="text-left mx-2 font-weight-bold">
+              {dishDetail.name}
+            </CardTitle>
+            <CardText className="text-left mx-2">
+              {dishDetail.description}
+            </CardText>
+          </Card>
+        </div>
+      );
+      return dishDetailComponent;
+    } else {
+      return <div></div>;
+    }
+  }
 
-    return (
-      <React.Fragment>
-        {dishDetailComponent}
-        {this.renderComments(this.props.dishDetail.comments)}
-      </React.Fragment>
-    );
+  render() {
+    if (this.props.dishDetail != null) {
+      return (
+        <div className="container">
+          <div className="row">
+            {this.renderDishDetail(this.props.dishDetail)}
+            {this.renderComments(this.props.dishDetail.comments)}
+          </div>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
   }
 }
 
